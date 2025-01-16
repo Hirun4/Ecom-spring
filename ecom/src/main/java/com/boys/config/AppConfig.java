e2a8489b6263b497666b60353401a71cfd89e492
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-16 14:54:55 +0530

    session management

--- ecom/src/main/java/com/boys/config/AppConfig.java
+++ ecom/src/main/java/com/boys/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.boys.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration 
@@ -10,6 +11,9 @@ public class AppConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeHttpRequests(Authorize->Authorize.requestMatchers("/api/**").authenticated().anyRequest())
         return null;
     }
 }
