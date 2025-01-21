f50fada0bb7f5ce72a1dd7db6c93c92bcba39567
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-21 09:53:59 +0530

    changes

--- ecom/src/main/java/com/boys/config/AppConfig.java
+++ ecom/src/main/java/com/boys/config/AppConfig.java
@@ -55,10 +55,11 @@ public class AppConfig {
                 }
                 ).and().httpBasic().and().formLogin();
 
-                
+
         return http.build();
     }
 
+    
 
     @Bean
     public PasswordEncoder passwordEncoder(){
